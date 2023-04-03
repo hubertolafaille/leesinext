a845ea8fb44fe97c30bfe52e647461745bca9874
hubertolafaille <120303136+hubertolafaille@users.noreply.github.com>
2023-04-03 11:02:14 +0200

    Storage of keywords added

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
+let keywords = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    // clearKeywords();
+    loadKeywords();
+    console.log(chrome.storage.sync.get('keywords'));
     const inputElement = document.getElementById('add-keyword-input');
     inputElement.focus();
+    document.getElementById('add-keyword-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        let keyword = document.getElementById('add-keyword-input').value;
+        addKeyword(keyword);
+        document.getElementById('add-keyword-input').value = '';
+    });
 });
 
-document.getElementById('add-keyword-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    let keyword = document.getElementById('add-keyword-input').value;
-    displayKeyword(keyword);
-    document.getElementById('add-keyword-input').value = '';
-});
-
-function displayKeyword(keyword) {
-    let displayArea = document.createElement('div');
-    displayArea.innerText = keyword;
-    document.body.appendChild(displayArea);
+function loadKeywords() {
+    chrome.storage.sync.get('keywords', function (data) {
+        if (data.keywords) {
+            keywords = data.keywords;
+        }
+    });
 }
+
+function addKeyword(keyword) {
+    keyword = keyword.trim().toLowerCase();
+
+    if (keyword === '') {
+        console.log('Cannot add an empty keyword.');
+        return;
+    }
+
+    if (keywords.includes(keyword)) {
+        console.log('This keyword is already in the list.');
+        return;
+    }
+
+    keywords.push(keyword);
+    chrome.storage.sync.set({ keywords: keywords }, function () {
+        console.log('Keyword added to the list.');
+    });
+    console.log(chrome.storage.sync.get('keywords'));
+}
+
+function clearKeywords() {
+    keywords = [];
+    chrome.storage.sync.set({ keywords: keywords }, function () {
+        console.log('Keywords list cleared.');
+    });
+}
+

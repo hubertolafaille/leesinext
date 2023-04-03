a4ef79bef0ebc00e3c5a8c2ce2e6a7b48e212347
hubertolafaille <120303136+hubertolafaille@users.noreply.github.com>
2023-04-03 22:50:33 +0200

    refactor

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,26 @@
 const uniqueVideoIds = new Set();
+const TITLE_SELECTOR = '#video-title';
+const PARENT_SELECTOR = 'ytd-rich-item-renderer';
+const THUMBNAIL_ANCHOR_SELECTOR = 'a#thumbnail';
+const DEBOUNCE_TIME = 500;
 
-function changeVideoTitles() {
-    const titleElements = document.querySelectorAll('#video-title');
+function collectVideoIds() {
+    const titleElements = document.querySelectorAll(TITLE_SELECTOR);
     titleElements.forEach((titleElement) => {
-        const parentElement = titleElement.closest('ytd-rich-item-renderer');
+        const parentElement = titleElement.closest(PARENT_SELECTOR);
         if (!parentElement) {
             console.warn("No parent element found");
             return;
         }
 
-        const thumbnailAnchor = parentElement.querySelector('a#thumbnail');
+        const thumbnailAnchor = parentElement.querySelector(THUMBNAIL_ANCHOR_SELECTOR);
         if (!thumbnailAnchor) {
             console.warn("No thumbnail anchor element found");
             return;
         }
 
-        const videoId = thumbnailAnchor.getAttribute('href').split('watch?v=')[1];
+        const searchParams = new URLSearchParams(thumbnailAnchor.search);
+        const videoId = searchParams.get('v');
 
         if (!uniqueVideoIds.has(videoId)) {
             uniqueVideoIds.add(videoId);
@@ -24,17 +29,29 @@ function changeVideoTitles() {
     });
 }
 
+function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
+
 function observeDOM() {
     const targetNode = document.querySelector('ytd-app');
-    if (!targetNode) {
+    const isHomepage = window.location.pathname === '/';
+
+    if (!targetNode || !isHomepage) {
         setTimeout(observeDOM, 500);
         return;
     }
 
+    const debouncedCollectVideoIds = debounce(collectVideoIds, DEBOUNCE_TIME);
+
     const observer = new MutationObserver((mutationsList) => {
         for (const mutation of mutationsList) {
             if (mutation.type === 'childList') {
-                changeVideoTitles();
+                debouncedCollectVideoIds();
             }
         }
     });

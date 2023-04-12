85dc81711c460ef5e35b456023373aa50bd896f5
hubertolafaille <120303136+hubertolafaille@users.noreply.github.com>
2023-04-12 21:44:28 +0200

    Request to Youtube API added

--- scripts/content.js
+++ scripts/content.js
@@ -3,6 +3,7 @@ const TITLE_SELECTOR = '#video-title';
 const PARENT_SELECTOR = 'ytd-rich-item-renderer';
 const THUMBNAIL_ANCHOR_SELECTOR = 'a#thumbnail';
 const DEBOUNCE_TIME = 500;
+const API_KEY = '';
 
 function injectStyles(css) {
     const style = document.createElement('style');
@@ -15,14 +16,73 @@ injectStyles(`
     filter: blur(30px);
     transition: filter 0.3s ease-in-out;
   }
+  .checked {
+    /* Vous pouvez ajouter des styles ici pour indiquer que la vidéo a été vérifiée */
+  }
 `);
+
+function checkTitle(title) {
+    if (title.includes('z')){
+        console.log(title);
+        return true;
+    }
+    return false;
+}
+
+function checkDescription(description) {
+    return false;
+}
+
+function checkTags(tags) {
+    return false;
+}
+
+function isKeywordFounded(title, description, tags){
+    if (checkTitle(title)) {
+        return true;
+    }
+
+    if (checkDescription(description)) {
+        return true;
+    }
+
+    if (checkTags(tags)) {
+        return true;
+    }
+
+    return false;
+}
+
+function fetchVideoDetails(videoId) {
+    const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet&key=${API_KEY}`;
+
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if (data.items.length > 0) {
+                const snippet = data.items[0].snippet;
+                // console.log(snippet);
+                // Vérifiez les détails de la vidéo et déterminez si vous souhaitez supprimer la classe 'blurred'
+                return snippet;
+            } else {
+                console.warn('No video details found for videoId:', videoId);
+            }
+        })
+        .catch((error) => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
 
 function collectVideoIds() {
     const titleElements = document.querySelectorAll(TITLE_SELECTOR);
     titleElements.forEach((titleElement) => {
         const parentElement = titleElement.closest(PARENT_SELECTOR);
-        if (!parentElement) {
-            console.warn("No parent element found");
+        if (!parentElement || parentElement.classList.contains("checked")) {
             return;
         }
 
@@ -41,6 +101,20 @@ function collectVideoIds() {
         if (!uniqueVideoIds.has(videoId)) {
             uniqueVideoIds.add(videoId);
             console.log(`ID: ${videoId}`);
+
+            // Requête API
+            fetchVideoDetails(videoId).then(value => {
+                // console.log(value.title);
+                // console.log(value);
+                // Vérifiez les conditions pour supprimer la classe 'blurred'
+                if (isKeywordFounded(value.title, value.description, value.tags)){
+                    parentElement.classList.add("checked");
+                    return;
+                }
+                // Si les conditions sont remplies, supprimez la classe 'blurred' pour le parentElement
+                parentElement.classList.remove('blurred');
+                parentElement.classList.add("checked");
+            });
         }
     });
 }
